fe7992e0dcb8b0f11283f1cd3cb4d8f4b3088da0
tjaime <thiagojaime@hotmail.com>
2022-03-11 00:51:47 -0300

    Implementação dos testes modo release

--- main_test.go
+++ main_test.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
@@ -10,10 +9,13 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/stretchr/testify/assert"
 	"github.com/tjaime/go-gin-api-rest/controller"
+	"github.com/tjaime/go-gin-api-rest/db"
+	"github.com/tjaime/go-gin-api-rest/model"
 )
 
 func SetupRotasTest() *gin.Engine {
 	rotas := gin.Default()
+	gin.SetMode(gin.ReleaseMode)
 	return rotas
 }
 
@@ -30,6 +32,51 @@ func TestVerificaSaudacaoStatusOk(t *testing.T) {
 	mockRsp := `{"API diz":"Olá, thiago. De boas?"}`
 	rspBody, _ := ioutil.ReadAll(rsp.Body)
 	assert.Equal(t, mockRsp, string(rspBody))
+}
 
-	fmt.Println(string(rspBody))
+func TestListarAlunosHandler(t *testing.T) {
+	db.ConnectDb()
+
+	// Mock
+	InsertlunoMock()
+	defer DeleteAlunoMock()
+
+	r := SetupRotasTest()
+	r.GET("/alunos", controller.ListarAlunos)
+
+	req, _ := http.NewRequest("GET", "/alunos", nil)
+	rsp := httptest.NewRecorder()
+	r.ServeHTTP(rsp, req)
+
+	assert.Equal(t, http.StatusOK, rsp.Code)
+}
+
+func TestFindAlunoByCpf(t *testing.T) {
+	db.ConnectDb()
+
+	// Mock
+	InsertlunoMock()
+	defer DeleteAlunoMock()
+
+	r := SetupRotasTest()
+	r.GET("/alunos/cpf/:cpf", controller.FindAlunoByCpf)
+	req, _ := http.NewRequest("GET", "/alunos/cpf/31317916832", nil)
+	rsp := httptest.NewRecorder()
+
+	r.ServeHTTP(rsp, req)
+
+	assert.Equal(t, http.StatusOK, rsp.Code)
+}
+
+var ID int
+
+func InsertlunoMock() {
+	aluno := model.Aluno{Nome: "Thiago", CPF: "31317916832", RG: "333831214"}
+	db.DB.Create(&aluno)
+	ID = int(aluno.ID)
+}
+
+func DeleteAlunoMock() {
+	var aluno model.Aluno
+	db.DB.Delete(&aluno, ID)
 }
